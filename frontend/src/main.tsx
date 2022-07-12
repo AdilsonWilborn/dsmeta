4b35d9640ba04c999e3bdcdb41b677d084057b52
AdilsonWilborn <adilson.wilborn@hotmail.com>
2022-07-12 02:54:38 -0300

    Project clean

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
+/* Estrutura básica de uma aplicação REACT */
+/* O REACT é uma biblioteca que vai ser responsável por processar as informações que estão no projeto e apresentar um resultado gráfico no navegador,
+ esse processamento chama-se renderização, o processo de converto informações em imagem é renderização  */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render( /* esse .render manda renderizar a aplicação na tela do navegador
+ele faz isso conersando com o documento html que está na raiz ''index.html'' lá no arquivo dentro de body tem um div com id="root"
+esse processo de renderização vai ta dentro daquele div marcado com root*/
   <React.StrictMode>
-    <App />
+    <App /> {/* um componente visual é uma função do javascript, no app.tsx tem a função com um comando para retornar algum conteúdo */}
   </React.StrictMode>
 )
